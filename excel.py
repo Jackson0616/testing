db554bdebf8a1c07a95a9f3290294b0091bae765
Jackson0616 <sunleizzzzz@163.com>
2019-12-26 16:56:00 +0800

    1

--- excel.py
+++ excel.py
@@ -4,7 +4,7 @@ import csv
 
 class Excel:
 
-    def open_cvs(self, file_path):
+    def open_csv(self, file_path):
         data_list = []
         data = open(file_path, 'r', encoding="utf-8")
         dataline = csv.reader(data)
@@ -12,8 +12,14 @@ class Excel:
             # csv中每一行被转换为了list形式并被赋给i
             data_list.append(data)
         return data_list
+
+
+    def save_excel(self,data_list):
+
+
     def run(self):
-        aibuddy_list=self.open_cvs(r"C:/Users/Administrator/Desktop/蜘蛛智联大数据产品1.5/aibuddy20191225")
+        file_path=r"C:/Users/Administrator/Desktop/蜘蛛智联大数据产品1.5/aibuddy20191225.csv"
+        aibuddy_list=self.open_csv(file_path)
         print(len(aibuddy_list))
 
 if __name__ == '__main__':
